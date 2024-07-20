07f23a168d536ab1393a67a7b499f224b6f4c286
Spome <spome.mw@protonmail.com>
2024-07-20 14:52:48 +0400

    change repo

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,7 @@ import { Buffer } from "buffer";
 if(!NativeModules.SmazReact) {
 
 	// Throw error
-	throw new Error("The package '@nicolasflamel/smaz-react' doesn't seem to be linked. Make sure: \n\n" + Platform.select({
+	throw new Error("The package '@mwcproject/smaz-react' doesn't seem to be linked. Make sure: \n\n" + Platform.select({
 		ios: "- You have run 'pod install'\n",
 		default: ""
 	}) + "- You rebuilt the app after installing the package\n- You are not using Expo managed workflow\n");
